dbdf88daf4f6a7d2e7ef7f81518c1ec04ec2cc42
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-08 23:43:52 +0300

    Update 2_4.js

--- 2_4.js
+++ 2_4.js
@@ -1,14 +1,14 @@
 /* function sayHello(name = "someone") {
-    console.log(`Hello, ${name}!`);
+    return `Hello, ${name}!`;
 }
 
-sayHello("Dasha");
-sayHello(); */
+console.log(sayHello("Dasha"));
+console.log(sayHello()); */
 
 let sayHello = name => {
     if (name == undefined) name = "someone";
-    console.log(`Hello, ${name}!`);
+    return `Hello, ${name}!`;
 }
 
-sayHello("Dasha");
-sayHello();
+console.log(sayHello("Dasha"));
+console.log(sayHello()); 
