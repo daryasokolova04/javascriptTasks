d6a708a4f836a9f69547fab65c56b5af7801d688
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-08 16:12:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var a = 5;
-let b = 10;
-const c = 13;
+{ 
+    const a = 100;
+    //a = 20; //нельзя изменить константу (без const a =)
+    console.log(a); 
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
 
-a = 0;
-b = 0;
-//нельзя менятьт константу
+{
+    //console.log(a) //a еще нет (из-за let)
+    let a = 15;
+    console.log(a); 
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
+{
+    console.log(a); //из-за var a уже создалось (undefined)
+    var a;
+    a = 10;
+    console.log(a);
+}
