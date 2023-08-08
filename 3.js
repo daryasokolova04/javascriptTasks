8c703141a6d68e779cee98633ebd04e77a6df829
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-08 22:50:57 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,8 @@
+'use strict'; 
+
+let productName = "молоко"
+let productPrice = 10
+let productQuantity = 5
+let resultCost = productPrice * productQuantity
+
+console.log(`Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽.`)
