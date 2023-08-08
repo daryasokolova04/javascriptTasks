afd7b6908345ef912d16e5b0805a992fc42f8011
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-08 23:27:52 +0300

    Create 2_3.js

---
+++ 2_3.js
@@ -0,0 +1,9 @@
+let arr = [1, 10, 15, -100, -23, 19, 15032];
+
+let newArr = [];
+for (var i =0; i < arr.length; i++) {
+    newArr.push(arr[i] * 1.25);
+}
+
+console.log(arr);
+console.log(newArr);
