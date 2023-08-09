8e238017cd125e1533db510c93aa39c2d2bae883
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-09 11:17:03 +0300

    Create 3_5.js

---
+++ 3_5.js
@@ -0,0 +1,15 @@
+const obj = {
+	age: 22,
+	favColor: 'red',
+	height: 188,
+	pet: 'dog',
+	money: 12300
+};
+
+let {age, favColor, height, pet, money} = obj;
+
+console.log(age);
+console.log(favColor);
+console.log(height);
+console.log(pet);
+console.log(money);
