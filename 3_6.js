e3b8cb45c0356b60f47b2d306c24cf8ddc405dd5
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-09 11:32:19 +0300

    Create 3_6.js

---
+++ 3_6.js
@@ -0,0 +1,31 @@
+const obj = {
+	age: 22,
+	favColor: 'red',
+	height: 188,
+	pet: 'dog',
+	money: 12300
+};
+
+const updateObj = {
+	age: 23,
+	favColor: 'blue',
+	money: 11450
+};
+
+var obj2 = obj;
+for (key1 in updateObj) {
+    for (key2 in obj2) {
+        if (key1 == key2) obj2[key2] = updateObj[key1];
+    }
+}
+
+console.log(obj2);
+/**
+ * {
+ *	age: 23,
+ *	favColor: 'blue',
+ *	height: 188,
+ *	pet: 'dog',
+ *	money: 11450
+ * }
+ */
