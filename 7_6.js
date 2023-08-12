10408bd34106cb4013a6c455a9d28bbc42b0eca0
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-12 18:48:51 +0300

    Create 7_6.js

---
+++ 7_6.js
@@ -0,0 +1,16 @@
+function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month + 1);
+    let newDate = new Date(date - 1);
+    return newDate.getDate();
+}
+
+
+
+// июнь 2027
+console.log(getLastDayOfMonth(2027, 5)); // 30
+
+// февраль 2027
+console.log(getLastDayOfMonth(2027, 1)); // 28
+
+// январь 2027
+console.log(getLastDayOfMonth(2027, 0)); // 31
