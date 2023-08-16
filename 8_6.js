df76c759734ee33410909f6168a7506aad51f4c5
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-16 20:51:47 +0300

    Create 8_6.js

---
+++ 8_6.js
@@ -0,0 +1,18 @@
+const boxFactory = {
+	type: 'box',
+	count: 0,
+	produce: function() {
+		this.count++;
+		return 'Box №' + this.count;
+	}
+}
+
+const produceBox = (produceFn) => {
+	const boxName = produceFn();
+	console.log(boxName);
+}
+
+for(let i = 0; i < 25; i++) {
+    console.log(boxFactory.produce())
+//	produceBox(boxFactory.produce);
+}
