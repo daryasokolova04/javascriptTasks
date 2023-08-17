18c77188df25253e0a2d8bde098b48774d62a1b7
daryasokolova04 <112976450+daryasokolova04@users.noreply.github.com>
2023-08-17 12:45:47 +0300

    Create 9_1.js

---
+++ 9_1.js
@@ -0,0 +1,17 @@
+console.log('a');
+new Promise((resolve, reject) => {
+	console.log('b');
+	setTimeout(() => {
+		console.log('c');
+		resolve();
+	}, 0);
+})
+  .then(() => console.log('d'));
+
+console.log('e');
+setTimeout(() => console.log('f'), 0);
+console.log('g');
+
+
+/* a b e g c d f
+*/
